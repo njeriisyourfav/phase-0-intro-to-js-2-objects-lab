86cc2a2dfadae44e912b97786e07936e997d2749
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-17 00:06:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution in this file!
+const employee = {
+  name: "Lilian Njeri",
+  streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, ObjKey, ObjValue) {
+  for (const key in employee) {
+    delete employee[key];
+    }
+
+    employee.name = "Sam";
+
+
+    let newEmployee = {
+      ...employee , 
+      streetAddress:"11 Broadway"
+    };
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    for (const key in employee) {
+        delete employee[key];
+    }
+
+    employee.name = "Sam";
+employee.streetAddress = "12 Broadway";
+return employee;
+ }  
+    
+function deleteFromEmployeeByKey(){
+    for(const key in employee){
+       delete employee[key];
+      }
+
+      employee.name = "Sam"
+      let newEmployee = {
+        ...employee
+      } 
+     delete newEmployee.name
+      return newEmployee
+
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    for (const key in employee) {
+         delete employee[key];
+        }
+
+        employee.name = "Sam";
+        let newEmployee = delete employee.name;
+        return employee
+    }
